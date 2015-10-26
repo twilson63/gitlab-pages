6d74bbc1b1a7ef9f896c3f82f4304390309986cb
Tom Wilson <tom@jackhq.com>
2015-10-25 21:02:19 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var body = require('body/json')
 var send = require('send-data/json')
 var sendError = require('send-data/error')
+var gitlab = require('gitlab')({
+  url: 'http://gitlab.chscoderdojo.com',
+  token: 'nP8649d8fqqxN5zCb3Aj'
+})
 
 var http = require('http')
 var HttpHashRouter = require('http-hash-router')
@@ -9,6 +13,18 @@ var router = HttpHashRouter()
 
 router.set('/api/system', (req, res) => {
   body(req, res, (err, body) => {
+    if (err) { return sendError(req, res, { body: err.message }) }
+    gitlab.projects.hooks.add(body.project_id, {
+      url: 'http://w3z.herokuapp.com/api/project',
+      push_events: true
+    })
+    send(req, res, { ok: true })
+  })
+})
+
+router.set('/api/project', (req, res) => {
+  body(req, res, (err, body) => {
+    if (err) { return sendError(req, res, { body: err.message }) }
     console.dir(body)
     send(req, res, { ok: true })
   })
