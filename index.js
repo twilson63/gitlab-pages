d5e413613146e13bd0dc96117f3014ab8d2da03d
Tom Wilson <tom@jackhq.com>
2015-10-27 21:48:51 -0400

    fixed issue with upsert

--- index.js
+++ index.js
@@ -46,16 +46,17 @@ router.set('/api/project', (req, res) => {
         to: 'index.html'
       }]
     }).then(function (result) {
+      console.log(result)
       var rev = result.rev
       gitlab.projects.repository.listTree(body.project_id, handle(''))
-
+      return true
       function handle (path) {
         return function (tree) {
           tree.forEach(function (node) {
             if (node.type === 'tree') {
               return gitlab.projects.repository.listTree(body.project_id, { path: node.name }, handle(node.name))
             }
-            request.get(`${apiUrl}/projects/16/repository/raw_blobs/${node.id}`, {
+            request.get(`${apiUrl}/projects/${body.project_id}/repository/raw_blobs/${node.id}`, {
               qs: {
                 private_token: config.token
               }
@@ -73,10 +74,10 @@ router.set('/api/project', (req, res) => {
           })
         }
       }
+    }).then(function () {
+      console.dir(body)
+      send(req, res, { ok: true })
     })
-
-    console.dir(body)
-    send(req, res, { ok: true })
   })
 })
 
@@ -86,23 +87,23 @@ var server = http.createServer((req, res) => {
 
 server.listen(process.env.PORT || 3000)
 
-function updateAttachments (projectId) {
-  function push (node) {
-    request.get(config.url + `/api/v3/projects/${projectId}/repository/raw_blobs/${node.id}?private_token={token}`, {
-      json: true
-    }).pipe(request.put(`${couchUrl}/${node.name}`, {}))
-  }
-
-  function handle (tree) {
-    tree.forEach(function (node) {
-      if (node.type === 'tree') {
-        return gitlab.projects.repository.listTree(projectId, {
-          path: node.name
-        }, handle)
-      }
-      if (node.type === 'blob') push(node)
-    })
-  }
-
-  gitlab.projects.repository.listTree(projectId, { path: '' }, handle)
-}
+// function updateAttachments (projectId) {
+//   function push (node) {
+//     request.get(config.url + `/api/v3/projects/${projectId}/repository/raw_blobs/${node.id}?private_token={token}`, {
+//       json: true
+//     }).pipe(request.put(`${couchUrl}/${node.name}`, {}))
+//   }
+//
+//   function handle (tree) {
+//     tree.forEach(function (node) {
+//       if (node.type === 'tree') {
+//         return gitlab.projects.repository.listTree(projectId, {
+//           path: node.name
+//         }, handle)
+//       }
+//       if (node.type === 'blob') push(node)
+//     })
+//   }
+//
+//   gitlab.projects.repository.listTree(projectId, { path: '' }, handle)
+// }
